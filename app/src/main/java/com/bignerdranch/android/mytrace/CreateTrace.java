9034d260aa33c2d7830b27cb61e7afe91b6e6de4
blying <1013018006@qq.com>
2016-12-06 13:28:46 +0800

    动画

---
+++ app/src/main/java/com/bignerdranch/android/mytrace/CreateTrace.java
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.mytrace;
+
+import android.app.Activity;
+
+/**
+ * Created by bly on 2016/12/6.
+ */
+
+public class CreateTrace extends Activity {
+
+}
--- app/src/main/java/com/bignerdranch/android/mytrace/RecentTraceActivity.java
+++ app/src/main/java/com/bignerdranch/android/mytrace/RecentTraceActivity.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,15 +27,21 @@ import java.util.List;
 public class RecentTraceActivity extends AppCompatActivity {
     private RecyclerView mRecentRecyclerView;
     private List<String> mTraceNameString=new ArrayList<>();
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    Toolbar mTbHeadBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         initData();
+        mDrawerLayout=(DrawerLayout)findViewById(R.id.drawerLayout);
+        mTbHeadBar=(Toolbar)findViewById(R.id.tbHeadBar);
         mRecentRecyclerView=(RecyclerView)findViewById( R.id.recentRecyclerView);
         mRecentRecyclerView.setAdapter(new RecentRecyclerViewAdapter(mTraceNameString));
         mRecentRecyclerView.setLayoutManager(new GridLayoutManager(this,2));
+
 
         NavigationView navigationView=(NavigationView)findViewById(R.id.navigation_view);
         navigationView.setItemIconTintList(null);
@@ -46,10 +55,9 @@ public class RecentTraceActivity extends AppCompatActivity {
                     startActivity(intent);
                 }
                 return false;
-
             }
         });
-
+        initToolBarAndDrawableLayout();
     }
     public void initData(){
         mTraceNameString.add("轨迹1");
@@ -60,6 +68,31 @@ public class RecentTraceActivity extends AppCompatActivity {
         mTraceNameString.add("轨迹6");
         mTraceNameString.add("轨迹7");
     }
+    private void initToolBarAndDrawableLayout() {
+        setSupportActionBar(mTbHeadBar);
+        /*以下俩方法设置返回键可用*/
+        getSupportActionBar().setHomeButtonEnabled(true);
+      getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        /*设置标题文字不可显示*/
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mTbHeadBar,R.string.app_name, R.string.app_name) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                //Toast.makeText(RecentTraceActivity.this, "open", Toast.LENGTH_SHORT).show();
+              }
+                    @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                //Toast.makeText(RecentTraceActivity.this, "close", Toast.LENGTH_SHORT).show();
+                }
+            };
+        /*mMyDrawable.setDrawerListener(mToggle);不推荐*/
+        mDrawerLayout.setDrawerListener(mActionBarDrawerToggle);
+        mActionBarDrawerToggle.syncState();/*同步状态*/
+       }
+
 class RecentRecyclerViewAdapter extends RecyclerView.Adapter<RecentRecyclerViewAdapter.MyViewHolder>{
     List<String> mTraceNameList;
     public RecentRecyclerViewAdapter(List<String> mTraceNameList) {
